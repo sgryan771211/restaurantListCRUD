63f0f0469f91f40271aa5d184d5165a8c80ff7e8
dongdong <sgryan771211@gmail.com>
2019-04-14 11:17:10 +0800

    modify restaurantList.js

--- models/restaurantList.js
+++ models/restaurantList.js
@@ -2,10 +2,46 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const restaurantListSchema = new Schema({
+  id: {
+    type: Number,
+    required: true,
+  },
   name: {
     type: String,
     required: true,
-  }
+  },
+  name_en: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  google_map: {
+    type: String,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
 })
 
 module.exports = mongoose.model('RestaurantList', restaurantListSchema)
