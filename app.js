9735b28c883ead027993926cf67adb4094fe3ed3
dongdong <sgryan771211@gmail.com>
2019-04-14 12:42:24 +0800

    Add route

--- app.js
+++ app.js
@@ -22,8 +22,43 @@ db.once('open', () => {
 const RestaurantList = require('./models/restaurantList');
 
 // 設定路由
+// restaurantList 首頁
 app.get('/', (req, res) => {
   res.send('hello')
+})
+// 列出全部 restaurant
+app.get('/restaurantList', (req, res) => {
+  res.send('列出所有 restaurant')
+})
+
+// 新增一筆 restaurant 頁面
+app.get('/restaurantList/new', (req, res) => {
+  res.send('新增 restaurant 頁面')
+})
+
+// 顯示一筆 restaurant 的詳細內容
+app.get('/restaurantList/:id', (req, res) => {
+  res.send('顯示 restaurant 的詳細內容')
+})
+
+// 新增一筆  restaurant
+app.post('/restaurantList', (req, res) => {
+  res.send('建立 restaurant')
+})
+
+// 修改 restaurant 頁面
+app.get('/restaurantList/:id/edit', (req, res) => {
+  res.send('修改 restaurant 頁面')
+})
+
+// 修改 restaurant
+app.post('/restaurantList/:id', (req, res) => {
+  res.send('修改 restaurant')
+})
+
+// 刪除 restaurant
+app.post('/restaurantList/:id/delete', (req, res) => {
+  res.send('刪除 restaurant')
 })
 
 // 設定 express port 3000
